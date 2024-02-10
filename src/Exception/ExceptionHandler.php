6e86dc2f4e8a6eb469e5a3c4f7f5a35452b2a3a7
Eko Prasetio <ekoxit1@gmail.com>
2024-02-10 15:58:32 +0900

    add : oauth2

---
+++ src/Exception/ExceptionHandler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Ekopras18\Satusehat\Exception;
+class ExceptionHandler
+{
+    public function response200($message, $data)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'code' => 200,
+            'data' => $data
+        ], 200);
+    }
+
+    public function response201($message)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'code' => 201
+        ], 201);
+    }
+
+    public function response401($message)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'code' => 401
+        ], 401);
+    }
+
+    public function response404($message)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'code' => 404
+        ], 404);
+    }
+
+    public function OperationOutcome($response)
+    {
+        $issue = $response['issue'][0];
+        $severity = $issue['severity'];
+        $code = $issue['code'];
+        $details = $issue['details']['text'];
+
+        return $this->response401("Error - Severity: $severity, Code: $code, Details: $details");
+    }
+
+    public function responseAuth($message,$response)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'code' => 200,
+            'data' => [
+                'api_product' => $response['api_product_list'],
+                'access_token' => $response['access_token'],
+                'token_type' => $response['token_type'],
+                'expires_in' => $response['expires_in']
+            ]
+        ], 200);
+    }
+}
--- src/Satusehat.php
+++ src/Satusehat.php
@@ -2,16 +2,11 @@
 
 namespace Ekopras18\Satusehat;
 
+use Ekopras18\Satusehat\Exception\ExceptionHandler;
+
 class  Satusehat
 {
-
-    public string $env;
-    public string $authUrl;
-    public string $baseUrl;
-    public string $consentUrl;
-    public string $clientId;
-    public string $clientSecret;
-    public string $organizationId;
+    public static $access_token = null;
 
     public function __construct()
     {
@@ -35,4 +30,52 @@ class  Satusehat
         $this->clientSecret = config('satusehat.client_secret');
         $this->organizationId = config('satusehat.organization_id');
     }
+
+    public function fhir()
+    {
+        return $this->$access_token;
+    }
+
+    public function generateToken()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->authUrl . '/accesstoken?grant_type=client_credentials',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => 'client_id=' . $this->clientId . '&client_secret=' . $this->clientSecret,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $response = json_decode($response, true);
+
+        if ($response === null) {
+            return ExceptionHandler::response401('Error - Failed to create token, please check your client_id and client_secret');
+        }
+
+        // Check structure of response
+        if (isset($response['resourceType']) && $response['resourceType'] === 'OperationOutcome') {
+
+            return ExceptionHandler::OperationOutcome($response);
+
+        } else {
+            self::$access_token = $response['access_token'];
+
+            return ExceptionHandler::responseAuth('Success Create Token', $response);
+        }
+    }
+
+
 }
