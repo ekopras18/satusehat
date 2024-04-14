71847499e2124776fa0da3f3875eaea02093b619
Eko Prasetio <ekoxit1@gmail.com>
2024-04-14 10:32:27 +0900

    optimize : code

--- tests/Test.php
+++ tests/Test.php
@@ -1,11 +1,26 @@
 <?php
 
+/**
+ * Namespace Satusehat\Integration\Tests
+ */
 namespace Satusehat\Integration\Tests;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class Test
+ *
+ * This class extends the TestCase class provided by PHPUnit. It is used to write unit tests for the application.
+ */
 class Test extends TestCase
 {
+    /**
+     * Test method for testing an example feature.
+     *
+     * Currently, this method simply returns a string 'coming soon...'. In the future, it should contain assertions to test the functionality of the application.
+     *
+     * @return string A string indicating that the test is yet to be implemented.
+     */
     public function testExample()
     {
         return 'coming soon...';
